ca9ae6bf10ab28a9e9d630eb03fbec6779da67b5
samuelmartineau <samuel.martineau@etu.utc.fr>
2013-10-18 16:41:29 +0200

    début projet tests

---
+++ Unity3D/Assets/Moves.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moves : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int speed = 5;
+		
+		if(Input.GetKey("up")){
+		Debug.Log ("Hello");
+		transform.Translate(Vector3.up*speed, Camera.main.transform);
+		}
+		
+	}
+}
