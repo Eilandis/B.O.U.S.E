f88950c0ba5c2d63af27d907deb9f59c9741a071
samuelmartineau <samuel.martineau@etu.utc.fr>
2013-11-06 19:54:09 +0100

    ninja commit

--- Unity3D/Assets/Moves.cs
+++ Unity3D/Assets/Moves.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Moves : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		int speed = 5;
+public class Moves : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 1;
+    
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.up * moveSpeed ,Camera.main.transform);
+        
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.down * moveSpeed);
 		
-		if(Input.GetKey("up")){
-		Debug.Log ("Hello");
-		transform.Translate(Vector3.up*speed, Camera.main.transform);
-		}
-		
-	}
+		if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * moveSpeed);
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed );
+        
+ /*       if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * );
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * );
+            
+  */
+    }
 }
