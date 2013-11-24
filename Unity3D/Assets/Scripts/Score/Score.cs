5fce479fd8c8e5da5a66eed1235507efe8948403
Cyril De Faria <cyrildefaria@gmail.com>
2013-11-24 02:26:28 +0100

    Y avait quelques trucs qui n'étaient pas passés dans le commit précédent

---
+++ Unity3D/Assets/Scripts/Score/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	
+	protected int score;
+	// Use this for initialization
+	void Start () {
+	
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	public void Add (int gain) {
+		score+=gain;
+	
+	}
+	
+	public int Get () {
+		return score;
+	
+	}
+}
+
+
